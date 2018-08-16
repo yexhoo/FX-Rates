afcdf8587f4cf276f80332141df2d7be73c797f1
nolasco <josue.nolasco@gmail.com>
2018-08-16 01:07:13 -0500

    calendar and chart printing was added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import './app.css';
 import {getSymbols, getHistorical} from './fixer/FixerApi';
+import {BuildChart} from './c3/HistoricalChart';
+
+import Calendar from 'react-calendar';
 
 class App extends Component{
+
+
+    state = {
+        date: new Date(),
+      }
+     
+    onChange = date =>{
+        console.log(date);
+        this.setState({ date });
+        this.getFxRatesHistorical(date);
+    } 
 
     getFxRatesSymbols(){
         let symbolsRequest = {
@@ -12,25 +26,42 @@ class App extends Component{
         getSymbols(symbolsRequest);
     }
 
-    getFxRatesHistorical() {
-
+    getFxRatesHistorical(date) {
+        console.log("*********",date);
         let historicalRequest ={
-            date : new Date(),
+            date : new Date(date),
             base : 'EUR',
-            symbols : ['USD', 'MXN'],
-            onSuccess: (response)=>console.log(response),
+            symbols : ['USD', 'MXN', 'AED', 'ARS','AUD','BTC', 'BRL', 'CNY', 'CZK'],
+            onSuccess: (response)=>{
+                console.log(response);
+                BuildChart(response.data);
+            },
             onError: (error)=>console.log(error)
         };
-        
         getHistorical(historicalRequest);
     }
 
     render(){
         return(
+
             <div>
-             <h1>FX Rates</h1>
-               <button type="button" onClick={this.getFxRatesSymbols}>Get Fx Rates Symbols</button>
-               <button type="button" onClick={this.getFxRatesHistorical}>Get Historical Fx Rates</button>         
+
+                <div>
+                    <Calendar
+                    onChange={this.onChange}
+                    value={this.state.date}
+                    />
+                    
+                </div>
+
+                <div>
+                    <h1>FX Rates</h1>
+                    <button type="button" onClick={this.getFxRatesSymbols}>Get Fx Rates Symbols</button>
+                    <button type="button" onClick={this.getFxRatesHistorical}>Get Historical Fx Rates</button>         
+                </div>
+                <div id="chart"></div>
+                
+             
             </div>
         )
     }
